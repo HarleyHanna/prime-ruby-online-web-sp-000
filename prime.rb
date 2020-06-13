d5ede8c6ddbb259031ac524efe627491d2b2a613
Harley Hanna <harleyhannawrit@gmail.com>
2020-06-13 14:56:04 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,26 @@
-# Add  code here!
+def prime?(int)
+  if int <= 1
+    return false
+  elsif int == 2
+    return true
+  elsif int.even? == true && int > 2
+    return false
+  else
+    range = (3..10000)
+    range.to_a
+    bool = false
+    while bool == false
+      range.each do |num|
+      if int % num == 0
+        bool = false
+        break
+      elsif
+        bool = true
+      end
+      end
+    end
+    bool
+      
+  end
+end
+    
