0030fb848f7ae437130742398f87fa0fe06e3064
Harley Hanna <harleyhannawrit@gmail.com>
2020-06-13 15:02:20 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,10 +1,6 @@
 def prime?(int)
   if int <= 1
     return false
-  elsif int == 2
-    return true
-  elsif int.even? == true && int > 2
-    return false
   else
     range = (3..int)
     range.to_a
