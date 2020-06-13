a6e4796afff0fa5dd9858e990606bc57bb6dd8ba
Harley Hanna <harleyhannawrit@gmail.com>
2020-06-13 15:06:07 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,12 +1,14 @@
-def prime?(int)
-  if int <= 1
-    return false
-  else
-    if (3...int).any? { |num| int % num == 0}
-      return false
-    else
-      return true
+def prime?(n)
+    return false if n <= 1
+    return true  if n == 2 || n == 3
+    return false if n % 2 == 0 || n % 3 == 0
+    i = 5
+    w = 2
+    while i * i <= n
+      return false if n % i == 0
+      i += w
+      w = 6 - w
     end
+    true
   end
-end
     
